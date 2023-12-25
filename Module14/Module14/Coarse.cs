4ca334e8b4fd78bf34cc5895ca83a1a079b3e35a
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-12-25 20:14:19 +0300

    Task 14.3.3

--- Module14/Module14/Coarse.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Module14
-{
-    internal class Coarse
-    {
-        public string Name { get; set; }
-        public DateTime StartDate { get; set; }
-    }
-}
--- Module14/Module14/Program.cs
+++ Module14/Module14/Program.cs
@@ -8,100 +8,54 @@ namespace Module14
     {
         static void Main(string[] args)
         {
-            List<Student> students = new List<Student>
-            {
-                 new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
-                 new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
-                 new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
-                 new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
-            };
-            var coarses = new List<Coarse>
-            {
-                new Coarse {Name="Язык программирования C#", StartDate = new DateTime(2020, 12, 20)},
-                new Coarse {Name="Язык SQL и реляционные базы данных", StartDate = new DateTime(2020, 12, 15)},
-            };
+            var phoneBook = new List<Contact>();
 
-            var studentAdd = from student in students
-                             where student.Age < 29
-                             where student.Languages.Contains("английский")
-                             from student2 in coarses
-                             where student2.Name.Contains("C#")
-                             select new
-                             {
-                                 Name = student.Name,
-                                 YearOfBirth = student.Age,
-                                 Coarse = student2.Name
-                             };
+            // добавляем контакты
+            phoneBook.Add(new Contact("Инорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
 
-            foreach (var student in studentAdd)
-                Console.WriteLine($"Студент: {student.Name} добален на курс {student.Coarse}");
-
-            var contacts = new List<Contact>()
-            {
-                new Contact() { Name = "Андрей", Phone = 7999945005 },
-                new Contact() { Name = "Сергей", Phone = 799990455 },
-                new Contact() { Name = "Иван", Phone = 79999675 },
-                new Contact() { Name = "Игорь", Phone = 8884994 },
-                new Contact() { Name = "Анна", Phone = 665565656 },
-                new Contact() { Name = "Василий", Phone = 3434 }
-            };
+            var sotrPhoneBook = phoneBook.OrderBy(x => x.Name).ThenBy(x => x.LastName);
 
             while (true)
             {
-                var keyChar = Console.ReadKey().KeyChar;
-                Console.Clear();
+                var input = Console.ReadKey().KeyChar;
+                var parsed = Int32.TryParse(input.ToString(), out int pageNum);
 
-                if (!Char.IsDigit(keyChar))
+                if (!parsed || pageNum < 1 || pageNum > 3)
                 {
-                    Console.WriteLine("Error, write 1");
+                    Console.WriteLine();
+                    Console.WriteLine("Неверное число");
                 }
                 else
                 {
-                    IEnumerable<Contact> page = null;
-
-                    switch(keyChar)
+                    Console.WriteLine();
+                    var pageContent = sotrPhoneBook.Skip((pageNum - 1) * 2).Take(2);
+                    foreach (var contact in pageContent)
                     {
-                        case ('1'):
-                            page = contacts.Take(2);
-                            break;
-                        case ('2'):
-                            page = contacts.Skip(2).Take(2);
-                            break;
-                        case ('3'):
-                            page = contacts.Skip(4).Take(2);
-                            break;
+                        Console.WriteLine(contact.Name + " " + contact.LastName + " " + contact.PhoneNumber);
                     }
-
-                    if (page == null)
-                    {
-                        Console.WriteLine("Error write 1 or 2 or 3");
-                        continue;
-                    }
-
-                    foreach (var contact in page)
-                        Console.WriteLine(contact.Name + " " + contact.Phone);
                 }
             }
         }
-
-
-
     }
 
-    internal class Contact
+    public class Contact
     {
-        public Contact()
+        public Contact(string name, string lastName, long phoneNumber, String email)
         {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
         }
 
-        public string Name { get; set; }
-        public long Phone { get; set; }
-    }
-
-    class Student()
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public List<string> Languages { get; set; }
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
     }
 }
